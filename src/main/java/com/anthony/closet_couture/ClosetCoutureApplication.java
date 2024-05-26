07136fb9f7e43fb921c42204ac16ddac2111546a
Anthony Pearson <anthonypearson@macbook-air.mynetworksettings.com>
2024-05-26 11:45:18 -0400

    spring initialized

---
+++ src/main/java/com/anthony/closet_couture/ClosetCoutureApplication.java
@@ -0,0 +1,13 @@
+package com.anthony.closet_couture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetCoutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosetCoutureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anthony/closet_couture/ClosetCoutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthony.closet_couture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosetCoutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
